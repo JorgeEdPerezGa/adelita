d90c5e6fc6d03b3984d2582bf0e0d27007bf28e1
Jorge Ed PerezGa <perezga@icloud.com>
2018-10-06 16:11:00 -0700

    add pages

---
+++ src/components/Resources/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Resources = () => {
+  return (
+    <p>Resources</p>
+  );
+};
+
+export default Resources;
---
+++ src/containers/Journal/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Journal extends Component {
+  render() {
+    return (
+      <div>
+        <p>Journal</p>
+      </div>
+    );
+  }
+}
+
+export default Journal;
